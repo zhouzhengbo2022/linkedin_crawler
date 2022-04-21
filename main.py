2a1087c166b5a2efbbe910f9c659a1eca36507cf
Zhou Zhengbo <zhengbozhou2022@u.northwestern.edu>
2022-04-21 09:14:12 -0500

    new version of main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import csv
+import pandas
+
 
 # Creating a webdriver instance
 #driver = webdriver.Chrome("Enter-Location-Of-Your-Web-Driver")
 # This instance will be used to log into LinkedIn
-driver = webdriver.Chrome('@path2_chromedriver')
+driver = webdriver.Chrome('/Users/zhouzhengbo/downloads/chromedriver')
 
-email = "@email_account"
-password = "@password"
+email = "zhengbozhou2022@u.northwestern.edu"
+password = "204@uYCH"
 # Opening linkedIn's login page
 driver.get("https://linkedin.com/uas/login")
+colnames = ['index', 'linkedin', 'name', 'cur', 'label','1','2','3']
+data = pandas.read_excel('linkedinfb.xlsx', names=colnames)
+
+profile_urls = data.linkedin.tolist()
 
 # waiting for the page to load
 time.sleep(5)
@@ -40,77 +47,87 @@ driver.find_element_by_xpath("//button[@type='submit']").click()
 # XPath used here.
 #profile_url = "https://www.linkedin.com/in/wanqin-chen-287309107/" https://www.linkedin.com/in/kenneth-z-yan/
 #profile_url = "https://www.linkedin.com/in/zhengbo-zhou-5b18411b6/"
-profile_url = "https://www.linkedin.com/in/janice-jackson-edd/"
-driver.get(profile_url)
+#profile_urls = ['https://www.linkedin.com/in/gianklo','https://www.linkedin.com/in/afrazsiddiqui','https://www.linkedin.com/in/liujiayi','https://www.linkedin.com/in/zhayujie','https://www.linkedin.com/in/ying-xu-28579435','https://www.linkedin.com/in/eduardoguillen','https://www.linkedin.com/in/kevin-han-90726847','https://www.linkedin.com/in/hao-zhang-06722436','https://www.linkedin.com/in/jocelyn-jing-yin-2032b290','https://www.linkedin.com/in/cmlilley','https://www.linkedin.com/in/asjankit','https://www.linkedin.com/in/dhivyamadhavan','https://www.linkedin.com/in/hetal-thakkar-a295057','https://www.linkedin.com/in/anthonyjameshinds','https://www.linkedin.com/in/dalia-cohn-alperovich-39bb4a6b','https://www.linkedin.com/in/ankurgupta1']
+details = ['Name','job','skills']
+with open('linkedin1.csv','w') as f:
+    write = csv.writer(f)
+    write.writerow(details)
+    for profile_url in profile_urls:
+        row = []
+        driver.get(profile_url)
 
-start = time.time()
+        start = time.time()
 
-# will be used in the while loop
-initialScroll = 0
-finalScroll = 1000
+        # will be used in the while loop
+        initialScroll = 0
+        finalScroll = 1000
 
-while True:
-    driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
-# this command scrolls the window starting from
-# the pixel value stored in the initialScroll
-# variable to the pixel value stored at the
-# finalScroll variable
-    initialScroll = finalScroll
-    finalScroll += 1000
+        while True:
+            driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
+        # this command scrolls the window starting from
+        # the pixel value stored in the initialScroll
+        # variable to the pixel value stored at the
+        # finalScroll variable
+            initialScroll = finalScroll
+            finalScroll += 1000
 
-    # we will stop the script for 3 seconds so that
-    # the data can load
-    time.sleep(3)
-    # You can change it as per your needs and internet speed
+            # we will stop the script for 3 seconds so that
+            # the data can load
+            time.sleep(3)
+            # You can change it as per your needs and internet speed
 
-    end = time.time()
+            end = time.time()
 
-    # We will scroll for 20 seconds.
-    # You can change it as per your needs and internet speed
-    if round(end - start) > 20:
-        break
+            # We will scroll for 20 seconds.
+            # You can change it as per your needs and internet speed
+            if round(end - start) > 20:
+                break
 
-src = driver.page_source
+        src = driver.page_source
 
-# Now using beautiful soup
-soup = BeautifulSoup(src, 'lxml')
+        # Now using beautiful soup
+        soup = BeautifulSoup(src, 'lxml')
 
-time.sleep(5)
+        time.sleep(5)
 
-intro = soup.find('div', {'class': 'pv-text-details__left-panel'})
-
-
-
-name_loc = intro.find("h1")
-
-# Extracting the Name
-name = name_loc.get_text().strip()
-# strip() is used to remove any extra blank spaces
-
-works_at_loc = intro.find("div", {'class': 'text-body-medium'})
-
-# this gives us the HTML of the tag in which the Company Name is present
-# Extracting the Company Name
-works_at = works_at_loc.get_text().strip()
-
-location_loc = intro.find_all("span", {'class': 'text-body-small'})
-
-# Ectracting the Location
-# The 2nd element in the location_loc variable has the location
-#location = location_loc[0].get_text().strip()
-
-print("Name -->", name,"\nWorks At -->", works_at,"\nLocation -->", location_loc)
-
-total = soup.find_all("section")
-#print(total)
-experiences = soup.find_all("span", {"class": "mr1 hoverable-link-text t-bold"})
+        intro = soup.find('div', {'class': 'pv-text-details__left-panel'})
 
 
-#print(experiences)
-for experience in experiences:
-    skills = experience.find_all('span',{'class':'visually-hidden'})
-    for skill in skills:
-        print(f'skill is {skill.get_text().strip()}')
+
+        name_loc = intro.find("h1")
+
+        # Extracting the Name
+        name = name_loc.get_text().strip()
+        # strip() is used to remove any extra blank spaces
+
+        works_at_loc = intro.find("div", {'class': 'text-body-medium'})
+
+        # this gives us the HTML of the tag in which the Company Name is present
+        # Extracting the Company Name
+        works_at = works_at_loc.get_text().strip()
+
+        location_loc = intro.find_all("span", {'class': 'text-body-small'})
+
+        # Ectracting the Location
+        # The 2nd element in the location_loc variable has the location
+        #location = location_loc[0].get_text().strip()
+
+        print("Name -->", name,"\nWorks At -->", works_at,"\nLocation -->", location_loc)
+        row.append(name)
+        row.append(works_at)
+        total = soup.find_all("section")
+        #print(total)
+        experiences = soup.find_all("span", {"class": "mr1 hoverable-link-text t-bold"})
+
+        skill_item = []
+        #print(experiences)
+        for experience in experiences:
+            skills = experience.find_all('span',{'class':'visually-hidden'})
+            for skill in skills:
+                print(f'skill is {skill.get_text().strip()}')
+                skill_item.append(skill.get_text().strip())
+        row.append(skill_item)
+        write.writerow(row)
 #skills = soup.find_all('span').get_text()
 #print(skills)
 #skills = soup.find_all("div", {"class": ""})
